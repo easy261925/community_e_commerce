fa9a7daf0ac814b8358b4aa964f4241873e96de3
greyu <gre_yu@163.com>
2018-03-22 15:04:54 +0800

    update goods reducer

--- src/reducers/goods.js
+++ src/reducers/goods.js
@@ -1,9 +1,12 @@
 import {
   FETCH_GOODS,
-  RECEIVE_GOODS
+  RECEIVE_GOODS,
+  SEARCH_GOODS,
+  RECEIVE_SEARCH
 } from '../actions/types'
 
 const initialState = {
+  searchs: [],
   goods: []
 }
 
@@ -12,11 +15,22 @@ export default (state = initialState, action = {}) => {
     case FETCH_GOODS:
       return {
         ...state,
+        goods: []
       }
     case RECEIVE_GOODS:
       return {
         ...state,
         goods: action.payload
+      }
+    case SEARCH_GOODS:
+      return {
+        ...state,
+        searchs: []
+      }
+    case RECEIVE_SEARCH:
+      return {
+        ...state,
+        searchs: action.payload
       }
     default:
       return state

280606dc38e306d028001b0dcfe36f30eb925754
greyu <gre_yu@163.com>
2018-03-23 12:37:32 +0800

    add citys.json, RED_COLOR

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,9 @@
+import areaData from './citys';
+export const areas = areaData.provinces;
 export const API = 'http://192.168.191.1:8080/user/v1';
 export const USER_ID = 'userId';
 export const TOKEN = 'token';
 export const PRIMARY_COLOR = '#3f51b5';
+export const RED_COLOR = '#f56c6c';
 export const RE_USERNAME = /^[a-zA-Z]\w{6,25}$/
 export const RE_PHONE = /^1\d{10}$/;

f0349c5e6609a94ecb250ef6a1e40616c4e63085
greyu <gre_yu@163.com>
2018-03-23 21:49:12 +0800

    update constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,5 +5,6 @@ export const USER_ID = 'userId';
 export const TOKEN = 'token';
 export const PRIMARY_COLOR = '#3f51b5';
 export const RED_COLOR = '#f56c6c';
+export const RED_COLOR_ACTIVE = '#ff5a5f';
 export const RE_USERNAME = /^[a-zA-Z]\w{6,25}$/
 export const RE_PHONE = /^1\d{10}$/;

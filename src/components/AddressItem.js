df5e75f34bc9088a75036ac8f6bf06c4b7270a74
greyu <gre_yu@163.com>
2018-03-23 12:36:54 +0800

    add AddressPicker Component

---
+++ src/components/AddressItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  View,
+} from 'native-base';
+
+export default class extends React.Component {
+  render() {
+    return (
+      <View>
+      </View>
+    )
+  }
+}
